4279d9c64fa214c14a3fd652676365fbb05d8bcf
Michael Kroth <mkroth65@gmail.com>
2018-08-14 16:32:49 -0500

    152 - passing dynamic parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from "../../node_modules/@angular/core";
+import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router'
 import { RecipiesComponent } from "./recipies/recipies.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeDetailComponent } from "./recipies/recipe-detail/recipe-detail.component";
 
 const appRoutes: Routes = [
 	{ path: '', redirectTo: '/recipes', pathMatch: 'full'},
-	{ path: 'recipes', component: RecipiesComponent},
+	{ path: 'recipes', component: RecipiesComponent, children: [
+		{ path: '' , component: RecipeStartComponent},
+		{ path: ':id' , component: RecipeDetailComponent}
+	]},
 	{ path: 'shopping-list', component: ShoppingListComponent}
 ]
 
--- src/app/recipies/recipe-detail/recipe-detail.component.ts
+++ src/app/recipies/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -8,11 +9,20 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-	@Input() recipe: Recipe
+	//@Input() recipe: Recipe
+	recipe: Recipe
+	id: number
 
-  constructor(private recipeService: RecipeService) { }
+	constructor(private recipeService: RecipeService,
+							//private router: Router,
+							private route: ActivatedRoute) { }
 
   ngOnInit() {
+		this.route.params.subscribe((params: Params) => {
+			this.id = +params['id']
+			this.recipe = this.recipeService.getRecipe(this.id)
+		})
+
 	}
 	
 	onAddToShoppingList() {
