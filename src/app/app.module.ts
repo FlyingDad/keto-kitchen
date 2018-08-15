aff78b71b1e87e89f28fcd40b6472f9316ab085b
Michael Kroth <mkroth65@gmail.com>
2018-08-15 15:16:46 -0500

    155 Edit recipe retrieving route params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import { Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
-import { RecipiesComponent } from './recipies/recipies.component';
-import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipiesComponent } from './recipes/recipies.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
 import { DropdownDirective } from './shared/dropdown.directive'
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 		ShoppingListComponent,
 		ShoppingEditComponent,
 		DropdownDirective,
-		RecipeStartComponent
+		RecipeStartComponent,
+		RecipeEditComponent
   ],
   imports: [
 		BrowserModule,
