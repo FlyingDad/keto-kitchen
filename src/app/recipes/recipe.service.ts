1221693016e18e4eb713c72c3a7c40f5793a28ee
Michael Kroth <mkroth65@gmail.com>
2018-09-08 22:22:22 -0500

    Added recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,27 +13,43 @@ export class RecipeService {
 		recipiesChanged = new Subject<Recipe[]>()
 
   	private recipes = [
-    new Recipe(
-      "Mac-n-Cheese",
-      "Everyones favorite meal.",
-			"http://assets.kraftfoods.com/recipe_images/opendeploy/52089_640x428.jpg",
-			[
-				new Ingredient('Elbow Macaroni', 16),
-				new Ingredient('Cheddar Cheese', 8),
-				new Ingredient('Cream', 1)
-			]
-    ),
-    new Recipe(
-      "Spaghetti and Cheese",
-      "The trailer park version. Bubble's loves it!",
-			"https://www.yellowblissroad.com/wp-content/uploads/2016/05/Creamy-Spaghetti-4.jpg",
-			[
-				new Ingredient('Great Value Spaghetti Noodles', 16),
-				new Ingredient('Great Value Cheddar Cheese', 8),
-				new Ingredient('Milk', 1)
-			]
-    )
-  ];
+			new Recipe(
+				"Fathead Pizza",
+				"Keto Pizza - A simple take on how to get your pizza fix without the carbs. It’s everything you want — pepperoni, cheese and tomato-sauce deliciousness.",
+				"https://blog.kettleandfire.com/wp-content/uploads/2017/06/keto-pizza.jpg",
+				[
+					new Ingredient('Eggs', 4),
+					new Ingredient('Tomato paste', 6),
+					new Ingredient('Shredded Mozarella Cheese', 6),
+					new Ingredient('Oregano', 1),
+					new Ingredient('Shredded Cheese', 5),
+					new Ingredient('Olives', 1),
+					new Ingredient('Leaf Spinach', 5),
+					new Ingredient('Olive Oil', 4),
+					new Ingredient('Salt and Pepper', 1)
+				]
+			),
+			new Recipe(
+				"Mac-n-Cheese",
+				"Everyones favorite meal.",
+				"http://assets.kraftfoods.com/recipe_images/opendeploy/52089_640x428.jpg",
+				[
+					new Ingredient('Elbow Macaroni', 16),
+					new Ingredient('Cheddar Cheese', 8),
+					new Ingredient('Cream', 1)
+				]
+			),
+			new Recipe(
+				"Spaghetti and Cheese",
+				"The trailer park version. Bubble's loves it!",
+				"https://www.yellowblissroad.com/wp-content/uploads/2016/05/Creamy-Spaghetti-4.jpg",
+				[
+					new Ingredient('Great Value Spaghetti Noodles', 16),
+					new Ingredient('Great Value Cheddar Cheese', 8),
+					new Ingredient('Milk', 1)
+				]
+			)
+		];
 
 	constructor(private shoppingListService: ShoppingListService) {}
 	
